03019d78a1b1307120dec411b97ee167d9c3bce7
João Pedro <joaof082@gmail.com>
2021-07-13 03:15:09 -0300

    Creating button component
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/Components/Buttons/ButtonLogin/ButtonLogin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import "./ButtonStyle.scss";
+
+export default function ButtonLogin() {
+  return (
+    <button className="button-login"> <p className="text-button-login"> Log in </p> </button>
+  )
+}
