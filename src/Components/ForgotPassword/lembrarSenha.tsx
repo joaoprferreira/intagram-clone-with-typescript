cec52243d5dbfa16e910a3891997e9bc59a171aa
João Pedro <joaof082@gmail.com>
2021-07-13 03:18:45 -0300

    finalization of forgotr pass word component
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/Components/ForgotPassword/lembrarSenha.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./lembrarSenha.scss"
+
+export default function lembrarSenha() {
+  return (
+    <div>      
+      <a href="top" className="esqueceu-senha">Forgot Password?</a>
+    </div>
+  )
+}
