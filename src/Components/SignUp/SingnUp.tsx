4d56cc3bbe37ca982fbd496242f20f7a3ea92bd2
João Pedro <joaof082@gmail.com>
2021-07-13 03:19:23 -0300

    Creating Account Creation Component
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/Components/SignUp/SingnUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import "./SignUpStyle.scss";
+
+export default function SingnUp() {
+  return (  
+    <span className="create-account">
+      Don't have an account?
+    <a href="top" className="link-create-account"> Sign up </a>
+  </span>
+  )
+}
